428c279e0faba7c8bf5b3bc4685bec61b4f22c3d
santi <santimezquita@hotmail.es>
2023-01-11 15:42:13 +0100

    updates

--- src/components/helpers/postNewUrl.js
+++ src/components/helpers/postNewUrl.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const postNewUrl = async (body) => {
   console.log(body)
-    const response = await axios.post(`${process.env.REACT_APP_API_URI}/api/urls/newregister`,body);
+    const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/urls/newregister`,body);
     console.log(response)
     return response
 }
